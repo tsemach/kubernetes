b40f6106199fe8d25c812dbf0e7bb375e05fbee1
Tsemach Mizrachi <tsemach.mizrachi@gmail.com>
2024-04-23 19:27:24 +0300

    add mongodb with storage class

--- cluster/nestjs/build.sh
+++ cluster/nestjs/build.sh
@@ -1,3 +1,5 @@
-docker build -t laptop-1:5000/nestjs:latest .
-docker push laptop-11:5000/nestjs:latest
+docker build -t nestjs:latest .
+docker tag nestjs:latest 192.168.122.1:5000/nestjs:latest 
+
+docker push 192.168.122.1:5000/nestjs:latest
 
--- cluster/nestjs/src/app.service.ts
+++ cluster/nestjs/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+const id = Math.floor(Math.random() * 1000);
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello World! id=${id}`;
   }
 }
